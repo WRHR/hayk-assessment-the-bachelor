f6286fae7c4c5c8ad8e854d12bb94ef344a47209
Will Reeves <wrhreeves@gmail.com>
2020-05-13 16:05:06 -0600

    All tests passed

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,48 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find do |person| 
+    if person["status"] == "Winner"
+      return person["name"].split(' ').shift
+    end
+  end
+end
+
+
+def all_contestants(data)
+  contestants = []
+  data.map do |season, people|
+    people.map do |contestant|
+      contestants << contestant
+    end
+  end
+  contestants
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  all_contestants(data).find do |contestant|
+    if contestant["occupation"] == occupation
+      return contestant["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  all_contestants(data).reduce(0) do |hometown_count, contestant|
+    contestant["hometown"] == hometown ? hometown_count + 1 : hometown_count
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  all_contestants(data).find do |contestant|
+    if contestant["hometown"] == hometown
+      return contestant["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = data[season].map { |contestant| contestant["age"].to_f }
+  average_age = (contestant_ages.sum / contestant_ages.length).round
+  #binding.pry
 end
